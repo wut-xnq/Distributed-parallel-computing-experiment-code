5897cb1d6ff5ffd718310adb483b7923363b62ff
wut-xnq <2244129587@qq.com>
2024-11-04 20:51:27 +0800

    Code

---
+++ quick_sort.cpp
@@ -0,0 +1,57 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void rands(int* data, int sum);
+void sw(int* a, int* b);
+int partition(int* a, int sta, int end);
+void quickSort(int* a, int sta, int end);
+void rands(int* data, int sum) {        //生成随机数 从 0 到 9999999
+    for (int i = 0; i < sum; i++) {
+        data[i] = rand() % 100000000;
+    }
+}
+
+void sw(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int* a, int sta, int end) {
+    int i = sta, j = end + 1;
+    int x = a[sta];
+    while (1) {
+        while (a[++i] < x && i < end);
+        while (a[--j] > x);
+        if (i >= j)
+            break;
+        sw(&a[i], &a[j]);
+    }
+    a[sta] = a[j];          //a[sta]最终位置是a[j];
+    a[j] = x;              //a[j]最终位置的值为x;
+    return j;
+}
+
+void quickSort(int* a, int sta, int end) {
+    if (sta < end) {
+        int mid = partition(a, sta, end);
+        quickSort(a, sta, mid - 1);         //左递归
+        quickSort(a, mid + 1, end);         //右递归
+    }
+}
+
+int main() {
+    const int n = 8000000;  // 数据量设为 8000000
+    int* data = (int*)malloc(sizeof(int) * n);
+
+    rands(data, n);
+
+    clock_t start = clock();
+    quickSort(data, 0, n - 1);
+    clock_t end = clock();
+
+    printf("串行线程快速排序处理时间 : %lf s\n", (double)(end - start) / CLOCKS_PER_SEC);
+
+    free(data);
+    return 0;
+}
---
+++ quick_sort_omp.cpp
@@ -0,0 +1,103 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+void rands(int* data, int sum);
+void sw(int* a, int* b);
+int partition(int* a, int sta, int end);
+void quickSort_parallel(int* a, int lenArray, int numThreads);
+void quickSort_parallel_internal(int* a, int left, int right, int cutoff);
+
+void rands(int* data, int sum) {
+    for (int i = 0; i < sum; i++) {
+        data[i] = rand() % 100000000;
+    }
+}
+
+void sw(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int* a, int sta, int end) {
+    int i = sta, j = end + 1;
+    int x = a[sta];
+    while (1) {
+        while (a[++i] < x && i < end);
+        while (a[--j] > x);
+        if (i >= j)
+            break;
+        sw(&a[i], &a[j]);
+    }
+    a[sta] = a[j];
+    a[j] = x;
+    return j;
+}
+
+// 并行快速排序
+void quickSort_parallel(int* a, int lenArray, int numThreads) {
+    int cutoff = 1000;
+#pragma omp parallel num_threads(numThreads)       
+    {
+#pragma omp single      
+        {
+            quickSort_parallel_internal(a, 0, lenArray - 1, cutoff);
+        }
+    }
+}
+// 并行快速排序的内部实现
+void quickSort_parallel_internal(int* a, int left, int right, int cutoff) {
+    int i = left, j = right;
+    int tmp;
+    int pivot = a[(left + right) / 2];
+
+    while (i <= j) {
+        while (a[i] < pivot)
+            i++;
+        while (a[j] > pivot)
+            j--;
+        if (i <= j) {
+            tmp = a[i];
+            a[i] = a[j];
+            a[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+
+    if ((right - left) < cutoff) {
+        if (left < j) {
+            quickSort_parallel_internal(a, left, j, cutoff);
+        }
+        if (i < right) {
+            quickSort_parallel_internal(a, i, right, cutoff);
+        }
+    }
+    else {
+#pragma omp task        
+        {
+            quickSort_parallel_internal(a, left, j, cutoff);
+        }
+#pragma omp task
+        {
+            quickSort_parallel_internal(a, i, right, cutoff);
+        }
+    }
+}
+
+int main() {
+    const int n = 8000000;  // 数据量设为 8000000
+    int* data = (int*)malloc(sizeof(int) * n);
+    int numThreads = 64; // 设定使用的线程数量
+
+    rands(data, n);
+
+    double omp_time_start = omp_get_wtime();
+    quickSort_parallel(data, n, numThreads);
+    double omp_time_end = omp_get_wtime();
+
+    printf("并行线程快速排序处理时间 : %lf s\n", omp_time_end - omp_time_start);
+
+    free(data);
+    return 0;
+}
